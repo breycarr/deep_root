658e2c27a78c87c8aa2e42e236beeabc5872c320
Laurence Taylor <ltaylorapp@gmail.com>
2019-07-10 17:30:38 +0100

    rewrites plant class, gitignores moisture_readings.txt file
    Co-authored-by: Laurence Taylor <ltaylorapp@gmail.com>
    Co-authored-by Oliver Brownlow <olliebrownlow@gmail.com>

--- lib/plant.py
+++ lib/plant.py
@@ -1,4 +1,24 @@
+import time
+from board import SCL, SDA
+import busio
+from adafruit_seesaw.seesaw import Seesaw
+hello = "hello"
+
 class Plant():
 
     def soil_moisture(self):
-        return 350
+
+        i2c_bus = busio.I2C(SCL, SDA)
+
+        ss = Seesaw(i2c_bus, addr=0x36)
+
+        while True:
+            moisture_level = ss.moisture_read()
+            # temp = ss.get_temp()
+            try:
+                f = open("moisture_readings.txt", "a")
+                f.write("moisture: " + str(moisture_level) + "\n")
+            finally:
+                f.close()	
+            print("moisture: " + str(moisture_level))
+            time.sleep(60)
