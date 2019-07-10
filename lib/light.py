3f515d27ddb975db27e00f44c13cd635e111c10c
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-07-10 15:24:37 +0100

    Feature added: light class sets up link with breadboard on creation of new light instance Switches red light on and blue light off when red is called, and vice versa when blue light is called All lights can be switched off when not in use
    Co-authored-by: Francesca Chater <francescachater@gmail.com>

--- lib/light.py
+++ lib/light.py
@@ -1,24 +1,27 @@
 import RPi.GPIO as GPIO 
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-redlight = 18
-bluelight = 11
-
-GPIO.setup(redlight, GPIO.OUT)
-GPIO.setup(bluelight, GPIO.OUT)
-
 class Light():
-         
-    def red():
-      print("Soil too dry")
-      GPIO.output(self.redlight, True)
-      GPIO.output(self.bluelight, False)
+            
+      def __init__(self, redlight = 18, bluelight = 11):
+            self.redlight = redlight
+            self.bluelight = bluelight
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(redlight, GPIO.OUT)
+            GPIO.setup(bluelight, GPIO.OUT)
 
-    def blue():
-      print("Soil too wet")
-      GPIO.output(self.bluelight, True)
-      GPIO.output(self.redlight, False)
+      def red(self):
+            GPIO.output(self.redlight, True)
+            GPIO.output(self.bluelight, False)
+            print("Soil too dry")
 
+
+      def blue(self):
+            print("Soil too wet")
+            GPIO.output(self.bluelight, True)
+            GPIO.output(self.redlight, False)
+            
+      def all_lights_off(self):
+            GPIO.output(self.bluelight, False)
+            GPIO.output(self.redlight, False)
