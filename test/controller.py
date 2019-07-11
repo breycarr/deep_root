610426e8bef5fbb1c72537e9e32cb2e826ef446d
Oliver <olliebrownlow@gmail.com>
2019-07-11 12:31:50 +0100

    Added light method to Controller class if...else statement calls appropriate light method
    Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com>
    Co-authored-by: Francesca Chater <francescachater@gmail.com>
    Co-authored-by: Laurence Taylor <ltaylorapp@gmail.com>

--- test/controller.py
+++ test/controller.py
@@ -3,6 +3,18 @@ class Controller():
 	def welcome(self):
 		return "Welcome to Deep Root"
 
-	def soil_moisture(self, obj_class):
-		plant = obj_class()
+
+	def soil_moisture(self, plant_class):
+		plant = plant_class()
 		return plant.soil_moisture()
+
+	
+	def light(self, soil_moisture, light_class):
+		light = light_class()
+		if soil_moisture > 800:
+			light.blue()
+		elif soil_moisture < 500:
+			light.red()
+		else: 
+			light.green()
+
--- test/test_controller.py
+++ test/test_controller.py
@@ -1,8 +1,17 @@
 from controller import Controller
 
-class MockPlant():
-	def soil_moisture(self):
-		return 900
+class MockLight():
+	def blue(self):
+		print("Soil too wet")
+	
+
+	def green(self):
+		print("Soil just right")
+
+
+	def red(self):
+		print("Soil too dry")
+
 
 class TestController(object):
 
@@ -12,5 +21,41 @@ class TestController(object):
 
 	
 	def test_soil_moisture_reading(self):
+		class MockPlant():
+			def soil_moisture(self):
+				return 900
 		controller = Controller()
 		assert controller.soil_moisture(MockPlant) == 900
+
+
+	def test_blue(self, capsys):
+		class MockPlant():
+			def soil_moisture(self):
+				return 900
+		controller = Controller()
+		soil_moisture = controller.soil_moisture(MockPlant)
+		controller.light(soil_moisture, MockLight)
+		captured = capsys.readouterr()
+		assert captured.out == "Soil too wet\n"
+
+
+	def test_red(self, capsys):
+		class MockPlant():
+			def soil_moisture(self):
+				return 300
+		controller = Controller()
+		soil_moisture = controller.soil_moisture(MockPlant)
+		controller.light(soil_moisture, MockLight)
+		captured = capsys.readouterr()
+		assert captured.out == "Soil too dry\n"
+
+
+	def test_green(self, capsys):
+		class MockPlant():
+			def soil_moisture(self):
+				return 650
+		controller = Controller()
+		soil_moisture = controller.soil_moisture(MockPlant)
+		controller.light(soil_moisture, MockLight)
+		captured = capsys.readouterr()
+		assert captured.out == "Soil just right\n"
