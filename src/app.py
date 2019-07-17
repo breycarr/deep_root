f59f6261fc56ade1461a6261ac0dcb6e6786015f
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-07-17 17:15:46 +0100

    Removed obsolete class files
    Co-authored-by: Laurence Taylor <ltaylorapp@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,16 +1,9 @@
 import time
 import eel
 import moisture_reading
-from controller import Controller
 from plant import Plant
 
 moisture_reading.create_table()
-app = Controller()
-
-print(app.welcome())
-
-moisture_reading = app.soil_moisture(Plant)
-print(moisture_reading)
 
 eel.init('web')
 eel.start('index.html')
--- src/controller.py
+++
@@ -1,8 +0,0 @@
-class Controller():
-
-    def welcome(self):
-        return "Welcome to Deep Root"
-
-    def soil_moisture(self, plant_class):
-        plant = plant_class()
-        return plant.soil_moisture()
