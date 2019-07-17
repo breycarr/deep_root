31dbc2e2eb4c5db143ee10a7fddc577f5fc84086
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-07-17 12:49:30 +0100

    Test removed: Controller Light functionality no longer a feature in current design Testing light via controller is obsolete, so tests have been removed.
    Co-authored-by: Laurence Taylor <ltaylorapp@gmail.com>

--- src/controller_test.py
+++ src/controller_test.py
@@ -24,33 +24,3 @@ class TestController(object):
                 return 900
         controller = Controller()
         assert controller.soil_moisture(MockPlant) == 900
-
-    def test_blue(self, capsys):
-        class MockPlant():
-            def soil_moisture(self):
-                return 900
-        controller = Controller()
-        soil_moisture = controller.soil_moisture(MockPlant)
-        controller.light(soil_moisture, MockLight)
-        captured = capsys.readouterr()
-        assert captured.out == "Soil too wet\n"
-
-    def test_red(self, capsys):
-        class MockPlant():
-            def soil_moisture(self):
-                return 300
-        controller = Controller()
-        soil_moisture = controller.soil_moisture(MockPlant)
-        controller.light(soil_moisture, MockLight)
-        captured = capsys.readouterr()
-        assert captured.out == "Soil too dry\n"
-
-    def test_green(self, capsys):
-        class MockPlant():
-            def soil_moisture(self):
-                return 650
-        controller = Controller()
-        soil_moisture = controller.soil_moisture(MockPlant)
-        controller.light(soil_moisture, MockLight)
-        captured = capsys.readouterr()
-        assert captured.out == "Soil just right\n"
