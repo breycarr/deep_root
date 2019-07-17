6c01cd36df3e3555005d4b6c09af9d073b77ad52
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-07-17 12:46:59 +0100

    Bug fix: touchscreen function Identified that touchscreen input was being disabled by light class. light class has been obsoleted and so removed from app and controller Deploying to RPi for active testing
    Co-authored-by: Laurence Taylor <ltaylorapp@gmail.com>

--- src/app.py
+++ src/app.py
@@ -3,23 +3,14 @@ import eel
 import moisture_reading
 from controller import Controller
 from plant import Plant
-from light import Light
 
 moisture_reading.create_table()
 app = Controller()
-plant = Plant()
 
 print(app.welcome())
 
 moisture_reading = app.soil_moisture(Plant)
-app.light(moisture_reading, Light)
 print(moisture_reading)
 
 eel.init('web')
 eel.start('index.html')
-
-# while True:
-# moisture_reading = app.soil_moisture(Plant)
-# app.light(moisture_reading, Light)
-# time.sleep(60)
-
--- src/controller.py
+++ src/controller.py
@@ -6,12 +6,3 @@ class Controller():
     def soil_moisture(self, plant_class):
         plant = plant_class()
         return plant.soil_moisture()
-
-    def light(self, soil_moisture, light_class):
-        light = light_class()
-        if soil_moisture > 800:
-            light.blue()
-        elif soil_moisture < 500:
-            light.red()
-        else:
-            light.green()
